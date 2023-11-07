331e9fb1331dfc2bb3db99378d90cbe1d19046b0
Eng-Prud <prudence.n826@gmail.com>
2023-11-07 19:26:05 +0300

    Codes on Custom shell

---
+++ lib.c
@@ -0,0 +1,129 @@
+#include "shell.h"
+
+/**
+ * _memcpy - copies info
+ * @newptr: destination ptr
+ * @ptr: source ptr
+ * @size: size of the new ptr
+ */
+void _memcpy(void *newptr, const void *ptr, unsigned int size)
+{
+	char *char_ptr = (char *)ptr;
+	char *char_newptr = (char *)newptr;
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		char_newptr[i] = char_ptr[i];
+}
+
+/**
+ * aux_itoa - Converts int to string.
+ * @n: type int no
+ * Return: String
+ */
+char *aux_itoa(int n)
+{
+	unsigned int n1;
+	int lenght = get_len(n);
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * (lenght + 1));
+	if (buffer == 0)
+		return (NULL);
+
+	*(buffer + lenght) = '\0';
+
+	if (n < 0)
+	{
+		n1 = n * -1;
+		buffer[0] = '-';
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	lenght--;
+	do {
+		*(buffer + lenght) = (n1 % 10) + '0';
+		n1 = n1 / 10;
+		lenght--;
+	}
+	while (n1 > 0)
+		;
+	return (buffer);
+}
+
+/**
+ * get_sigint - Handle the crtl + c call in prompt
+ * @sig: Signal handler
+ */
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
+
+/**
+ * get_len - Get the lenght of a number.
+ * @n: type int number.
+ * Return: Lenght of a number.
+ */
+int get_len(int n)
+{
+	unsigned int n1;
+	int lenght = 1;
+
+	if (n < 0)
+	{
+		lenght++;
+		n1 = n * -1;
+	}
+	else
+	{
+		n1 = n;
+	}
+	while (n1 > 9)
+	{
+		lenght++;
+		n1 = n1 / 10;
+	}
+
+	return (lenght);
+}
+
+/**
+ * without_comment - deletes comments from the input
+ *
+ * @in: input string
+ * Return: input without comments
+ */
+char *without_comment(char *in)
+{
+	int i, up_to;
+
+	up_to = 0;
+	for (i = 0; in[i]; i++)
+	{
+		if (in[i] == '#')
+		{
+			if (i == 0)
+			{
+				free(in);
+				return (NULL);
+			}
+
+			if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
+				up_to = i;
+		}
+	}
+
+	if (up_to != 0)
+	{
+		in = _realloc(in, i, up_to + 1);
+		in[up_to] = '\0';
+	}
+
+	return (in);
+}
+
